c80fa51fb6ff34e0921503ca78ea96d5882842f5
lel-amri <lel-amri@student.42.fr>
2014-11-09 17:39:10 +0100

    Test d'affichage de list par recursion de template

--- intra/forum/views.py
+++ intra/forum/views.py
@@ -1,3 +1,11 @@
+from django import template
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    render(request, 'forum/index.html', {'cat': None})
+
+register = template.Library()
+@register.inclusion_tag('forum/cat_list.html', name='cat_list')
+def cat_list(cat):
+    cat_list = cat.objects.filter(parent=cat).order_by('name')
+    return {'cat_list': cat_list}
