f3a1fc13d4f3f45133a2aeebc0c2c63e79c2eb44
lel-amri <lel-amri@student.42.fr>
2014-11-11 13:05:04 +0100

    Ajout de 4 vues : new_thread, thread, category, reply

--- intra/forum/urls.py
+++ intra/forum/urls.py
@@ -5,4 +5,10 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^thread/(?P<thread_id>\d+)/$', views.thread, name='thread'),
     url(r'^thread/(?P<thread_id>\d+)/reply/$', views.reply, name='reply'),
+    url(r'^category/(?P<cat_id>\d+)/$', views.category, name='category'),
+    url(
+        r'^category/(?P<cat_id>\d+)/new/$',
+        views.new_thread,
+        name='new_thread'
+    ),
 )
--- intra/forum/views.py
+++ intra/forum/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from forum.models import Post
+from forum.models import Post, Category
 
 @login_required
 def index(request):
@@ -18,7 +18,10 @@ def thread(request, thread_id):
     except:
         raise Http404
     old_post = request.session.get('_old_post')
-    form = ReplyForm(old_post)
+    if old_post:
+        del request.session['_old_post']
+    post = Post(cat=thread.cat, parent=thread, author=request.user)
+    form = PostForm(old_post, instance=post)
     context = {
         'thread': thread,
         'form': form,
@@ -28,24 +31,57 @@ def thread(request, thread_id):
 @login_required
 def reply(request, thread_id):
     thread = get_object_or_404(Post, id=thread_id)
+    post = Post(cat=thread.cat, parent=thread, author=request.user)
     if request.method == 'POST':
-        old_post = request.session['_old_post']
-        del request.session['_old_post']
-        form = ReplyForm(old_post or None)
-        form.cat = thread.cat
-        form.parent = thread
-        form.author = request.user
-        form.title = None
+        request.session['_old_post'] = request.POST
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            return HttpResponse('Ok')
+            del request.session['_old_post']
+            form.save()
+            return HttpResponseRedirect(reverse(
+                'forum:thread',
+                args=[thread_id]
+            ))
     return HttpResponseRedirect(reverse('forum:thread', args=[thread_id]))
 
-class ReplyForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['message']
+@login_required
+def category(request, cat_id):
+    try:
+        cat = Category.objects.get(id=cat_id)
+    except:
+        raise Http404
+    threads = Post.objects.filter(cat=cat, parent=None)
+    context = {
+        'cat': cat,
+        'threads': threads,
+    }
+    return render(request, 'forum/category.html', context)
 
-class ThreadForm(forms.ModelForm):
+@login_required
+def new_thread(request, cat_id):
+    try:
+        cat = Category.objects.get(id=cat_id)
+    except:
+        raise Http404
+    thread = Post(cat=cat, parent=None, author=request.user)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=thread)
+        if form.is_valid():
+            thread = form.save()
+            return HttpResponseRedirect(reverse(
+                'forum:thread',
+                args=[thread.id]
+            ))
+    else:
+        form = PostForm(instance=thread)
+    context = {
+        'cat': cat,
+        'form': form,
+    }
+    return render(request, 'forum/new_thread.html', context)
+
+
+class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'message']
