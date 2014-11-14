b5e600190ec0e0eb62d6a4be07c5ec396f5d5976
Leo EL-AMRI <lel-amri@e3r2p19.42.fr>
2014-11-14 13:50:32 +0100

    retrait du virtualenv

--- ENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Volumes/Data/nfs/zfs-student-3/users/2013/lel-amri/working/web1/intra/ENV/bin/python3.4
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- ENV/lib/python3.4/weakref.py
+++
@@ -1,1 +0,0 @@
-/Volumes/Data/nfs/zfs-student-3/users/2013/lel-amri/.brew/Cellar/python3/3.4.1/Frameworks/Python.framework/Versions/3.4/lib/python3.4/weakref.py
