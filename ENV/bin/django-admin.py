59ce3968e043cd9112ea2f8ffb89136e759634ca
Hana KHELIFA <hkhelifa@e3r2p15.42.fr>
2014-11-28 15:28:56 +0100

    suppression de l'ENV

--- ENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Volumes/Data/nfs/zfs-student-2/users/2013/hkhelifa/intra/ENV/bin/python3.4
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
