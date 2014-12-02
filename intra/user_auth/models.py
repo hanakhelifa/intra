6603df11bf0b37ab1242e65159f92ebf2e628e8b
Marianne PORCEL <mporcel@e3r2p9.42.fr>
2014-12-02 17:27:24 +0100

    debut de User

--- intra/user_auth/models.py
+++ intra/user_auth/models.py
@@ -1,15 +1,32 @@
 from django.db import models
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
 
-class User(models.Model):
-	uid = models.CharField(max_length=8)
+class User(AbstractBaseUser):
+	uid = models.CharField(max_length=8, unique=True, primary_key=True)
 	password = models.CharField(max_length=20)
 	first_name = models.CharField(max_length=30)
 	last_name = models.CharField(max_length=30)
 	birth_date = models.DateField(auto_now=False, auto_now_add=False)
 	promo = models.ForeignKey('Promo')
-	is_staff = models.BooleanField(default=False)
-	is_superuser = models.BooleanField(default=False)
-#	is_ldap
+	USERNAME_FIELD = 'uid'
+	REQUIRED_FIELDS = ['first_name', 'last_name', 'birth_date', 'promo']
+
+	def user_login(request):
+		if request.uid.is_authenticated():
+			return HttpResponse('user authenticated')
+		if request.method == 'POST':
+			form = AuthenticationForm(data=request.POST)
+			if form.is_valid():
+				 uid = authenticate(username=form_data['uid'], password=form_data['password'])
+
+
 
 class Promo(models.Model):
 	year = models.CharField(max_length=4)
+
+
+
