c81f506a6d33fe3d2b29f5b0b7bf917fd57e8ad5
lel-amri <leo.el.amri@gmail.com>
2014-12-11 21:57:16 +0100

    Ajout d'un morceau de vue

--- intra/tickets/views.py
+++ intra/tickets/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
+from ticket.models import Ticket
 
-# Create your views here.
+@require_login
+def pool(request):
+    if not request.user.is_superuser:
+        return HttpResponse("You don't have access")
