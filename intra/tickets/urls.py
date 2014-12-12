a6147d42d3d74783c49b06843d8fbe70a3e4a63a
lel-amri <leo.el.amri@gmail.com>
2014-12-12 15:28:08 +0100

    Ajout d'une vue

--- intra/tickets/urls.py
+++ intra/tickets/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from tickets import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.pool),
+    url(r'^pool/$', views.pool, name='pool'),
+    url(r'^$', views.tickets_list, name='list'),
+    url(r'^new/$', views.create, name='create'),
 )
--- intra/tickets/views.py
+++ intra/tickets/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from tickets.models import Ticket, Status
@@ -12,3 +13,11 @@ def pool(request):
         .order_by('-last_event_date')
     )
     raise Http404
+
+@login_required
+def tickets_list(request):
+    tickets = (Ticket.objects
+        .filter(Q(last_assigned=request.user) | Q(author=request.user))
+        .order_by('status', '-last_event_date')
+    )
+    return render(request, 'tickets/tickets_list.html', {'tickets': tickets})
