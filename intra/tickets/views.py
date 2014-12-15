4062d74a62a8b5dcbacf1984d5b9d6f3b1c47e71
lel-amri <leo.el.amri@gmail.com>
2014-12-15 16:52:51 +0100

    Ajout d'un peu de vue

--- intra/tickets/views.py
+++ intra/tickets/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django.db.models import Q
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from tickets.forms import TicketForm, MessageForm, AssignForm
 from tickets.models import Ticket, Status
@@ -9,17 +10,29 @@ from tickets.models import Ticket, Status
 def pool(request):
     if not request.user.is_superuser:
         return HttpResponse("You don't have access")
-    tickets = (Ticket.objects
+    tickets_open = (Ticket.objects
         .filter(last_status=Status.OPEN)
         .order_by('-last_event_date')
     )
-    raise Http404
+    tickets_close = (Ticket.objects
+            .filter(last_status=Status.CLOSE)
+        .order_by('-last_event_date')
+    )
+    return render(request, 'tickets/pool.html',
+        {
+            'opened_tickets': tickets_open,
+            'closed_tickets': tickets_close
+        }
+    )
 
 @login_required
 def tickets_list(request):
     tickets = (Ticket.objects
-        .filter(Q(last_assigned=request.user) | Q(author=request.user))
-        .order_by('status', '-last_event_date')
+        .filter(
+            (Q(last_assigned=request.user) & ~Q(author=request.user))
+            | Q(author=request.user)
+        )
+        .order_by('last_status', '-last_event_date')
     )
     return render(request, 'tickets/tickets_list.html', {'tickets': tickets})
 
@@ -29,11 +42,15 @@ def create(request):
         form_ticket = TicketForm(request.POST)
         form_message = MessageForm(request.POST)
         if form_ticket.is_valid() and form_message.is_valid():
-            Ticket.create(
+            ticket = Ticket.create(
                 title=form_ticket.cleaned_data['title'],
                 message=form_message.cleaned_data['message'],
                 author=request.user
             )
+            return HttpResponseRedirect(reverse(
+                'tickets:view',
+                args=[ticket.pk, ]
+            ))
     else:
         form_ticket = TicketForm()
         form_message = MessageForm()
@@ -49,11 +66,47 @@ def create(request):
 @login_required
 def view(request, ticket_id):
     ticket = get_object_or_404(Ticket, pk=ticket_id)
-    form = MessageForm()
-    if request.user.is_superuser:
-        assign_form = AssignForm()
+    if (
+        request.user != ticket.author
+        and request.user != ticket.last_assigned
+        and not request.user.is_superuser
+    ):
+        return Http404
+    assign_form = None
+    if request.method == "POST":
+        if 'open' in request.POST:
+            ticket.open(request.user)
+            form = MessageForm()
+            if request.user.is_superuser:
+                assign_form = AssignForm()
+        else:
+            form = MessageForm(request.POST)
+            if request.user.is_superuser:
+                assign_form = AssignForm(request.POST)
+            if form.is_valid() or assign_form.is_valid():
+                if form.is_valid():
+                    ticket.add_message(
+                        author=request.user,
+                        message=form.cleaned_data['message']
+                    )
+                    form = MessageForm()
+                if assign_form.is_valid():
+                    assign = ticket.assign(
+                        author=request.user,
+                        to=assign_form.cleaned_data['assigned_to']
+                    )
+            form = MessageForm()
+            if request.user.is_superuser:
+                assign_form = AssignForm()
+        if 'close' in request.POST:
+            ticket.close(request.user)
+            form = MessageForm()
+            if request.user.is_superuser:
+                assign_form = AssignForm()
     else:
-        assign_form = None
+        form = MessageForm()
+        if request.user.is_superuser:
+            assign_form = AssignForm()
     events = ticket.get_events()
     return render(
         request,
